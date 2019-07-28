e323cce811f7dc0e4ea184428677d2878e6f8af0
Mahtab Alam <Mahtab.Alam@ge.com>
2019-07-28 21:27:32 +0530

    Added Sudoku implementation

---
+++ src/net/mahtabalam/sudoku1/App.java
@@ -0,0 +1,23 @@
+package net.mahtabalam.sudoku1;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int sudokuMatrix[][] = {{5, 3, 0,  0, 7, 0,  0, 0, 0},
+				        		{6, 0, 0,  1, 9, 5,  0, 0, 0},
+				        		{0, 9, 8,  0, 0, 0,  0, 6, 0},
+				        		
+				        		{8, 0, 0,  0, 6, 0,  0, 0, 3},
+				        		{4, 0, 0,  8, 0, 3,  0, 0, 1},
+				        		{7, 0, 0,  0, 2, 0,  0, 0, 6},
+				        		
+				        		{0, 6, 0,  0, 0, 0,  2, 8, 0},
+				        		{0, 0, 0,  4, 1, 9,  0, 0, 5},
+				        		{0, 0, 0,  0, 8, 0,  0, 7, 9} };
+
+		Sudoku sudoku = new Sudoku(sudokuMatrix);
+		sudoku.solveSudoku();	
+	}
+}
---
+++ src/net/mahtabalam/sudoku1/Sudoku.java
@@ -0,0 +1,99 @@
+package net.mahtabalam.sudoku1;
+
+
+public class Sudoku {
+
+	private int[][] sudokuMatrix;
+	
+	public static final int BOARD_SIZE = 9;
+	public static final int BOX_SIZE = 3;
+	public static final int MIN_NUMBER = 1;
+	public static final int MAX_NUMBER = 9;
+	
+
+	public Sudoku(int[][] sudokuMatrix) {
+		this.sudokuMatrix = sudokuMatrix;
+	}
+
+	public void solveSudoku() {
+		
+		if (solve(0, 0)){
+			printResult();
+		} else {	
+			System.out.println("No feasible solution found...");
+		}
+	}
+
+	private boolean solve(int rowIndex, int columnIndex) {
+		
+		// Move to the first Row
+		if( rowIndex == BOARD_SIZE){
+			columnIndex = columnIndex + 1;
+			if(columnIndex == BOARD_SIZE) {
+				return true;
+			}
+			rowIndex=0;
+		}
+				
+		
+		if ( sudokuMatrix[rowIndex][columnIndex] != 0 ) { // skip filled cells
+			return solve(rowIndex + 1, columnIndex);
+		}
+
+		for (int number = MIN_NUMBER; number <= MAX_NUMBER; number++) {
+			
+			if ( isValid(rowIndex, columnIndex, number) ) {
+				
+				sudokuMatrix[rowIndex][columnIndex] = number;
+				
+				if ( solve(rowIndex + 1, columnIndex) )
+					return true;
+			}
+		}
+		
+		//  BACKTRACK !!!
+		sudokuMatrix[rowIndex][columnIndex] = 0; 
+		
+		return false;
+	}
+
+	private boolean isValid(int rowIndex, int columnIndex, int number) {
+		
+		// check if the same row alreday have that same digit
+		for (int i = 0; i < BOARD_SIZE; i++) 
+			if ( sudokuMatrix[rowIndex][i] == number )
+				return false;
+		
+		// check if the same column alreday have that same digit
+		for (int j = 0; j < BOARD_SIZE; j++) 
+			if ( sudokuMatrix[j][columnIndex] == number )
+				return false;
+
+		// check if the same subgrid(box) alreday have that same digit
+		int boxRowOffset = (rowIndex / 3) * BOX_SIZE;
+		int boxColumnOffset = (columnIndex / 3) * BOX_SIZE;
+		
+		for (int i = 0; i < BOX_SIZE; i++)
+			for (int j = 0; j < BOX_SIZE; j++)
+				if (sudokuMatrix[boxRowOffset + i][boxColumnOffset + j] == number)
+					return false;
+
+		return true;
+	}
+
+	private void printResult() {
+		
+		for (int i = 0; i < BOARD_SIZE; ++i) {
+			
+			if(i % 3 == 0) System.out.println(" ");
+			
+			for (int j = 0; j < BOARD_SIZE; ++j) {
+				
+				if (j % 3 == 0) System.out.print(" ");
+				System.out.print(sudokuMatrix[i][j]+" ");
+			}
+			
+			System.out.println(" ");
+		}	
+	}
+}
